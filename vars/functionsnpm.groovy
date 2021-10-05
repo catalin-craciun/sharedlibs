7feb1c34dca20eb57f4b40eddaa5fbe13564b380
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-05 21:31:17 +0300

    Update functionsnpm.groovy

--- vars/functionsnpm.groovy
+++ vars/functionsnpm.groovy
@@ -1,1 +1,13 @@
+def buildImagenpm(){
+  
+    myapp = docker.build("catalin088/nodejs-test:${env.BUILD_ID}")
+}
 
+def pushImagenpm() {
+ script {
+   docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+    myapp.push("latest")
+    myapp.push("${env.BUILD_ID}")
+     }
+ }
+}
