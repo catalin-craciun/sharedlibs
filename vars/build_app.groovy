a6f11524872cd25ec97a5a34b562374ca85e8c84
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-05 10:59:52 +0300

    Update build_app.groovy

--- vars/build_app.groovy
+++ vars/build_app.groovy
@@ -1,7 +1,7 @@
 def call(){
     sh '''
         export DOCKERID=catalin088
-        chmod 760 /var/run/docker.sock
+        whoami
         docker container run \
         --volume //var/run/docker.sock:/var/run/docker.sock \
         --detach \
