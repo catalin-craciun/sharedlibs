bc445572aa2c7b0ff5169fbb96b63b0953dafa7c
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-05 21:43:27 +0300

    Update mavenPipeline.groovy

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -1,1 +1,38 @@
 
+pipeline {
+  agent any
+
+  tools {
+    maven 'mvn-version'
+  }
+
+  stages {
+    stage('Build') {
+      steps {
+        sh 'mvn package'
+      }
+    }
+    
+    stage("Build image") {
+            steps {
+                script {
+                    echo "Build image with tag: ${env.BUILD_ID}"
+                    myapp = docker.build("catalin088/ledger-service:${env.BUILD_ID}", "--build-arg VERSION='${env.BUILD_ID}' .")
+                }
+            }
+        }
+    
+
+    stage("Push image") {
+        steps {
+                script {
+                    docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+                            myapp.push("latest")
+                            myapp.push("${env.BUILD_ID}")
+                    }
+               }
+          }
+     }
+ }
+  
+}
