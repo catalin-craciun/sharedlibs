d32599557885e058f071e7d34c67688d9ed54baa
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-05 13:23:54 +0300

    Update k8sPipeline.groovy

--- vars/k8sPipeline.groovy
+++ vars/k8sPipeline.groovy
@@ -17,7 +17,7 @@ def call() {
         }
         stage("Push image") {
             steps {
-                pushImage()
+                pushImage(myapp)
                 }
             }
   }        
