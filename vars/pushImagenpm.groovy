43574b5f347d48cf8835a4db56f3e9a563c34bc5
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-06 15:42:32 +0300

    Update pushImagenpm.groovy

--- vars/pushImagenpm.groovy
+++ vars/pushImagenpm.groovy
@@ -1,8 +1,8 @@
 def call() {
   script{
     docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
-      buildImagenpm.myapp.push("latest")
-      buildImagenpm.myapp.push("${env.BUILD_ID}")
+      buildImagenpm.myappmethod.push("latest")
+      buildImagenpm.myappmethod.push("${env.BUILD_ID}")
      }
   }
 }
