f1935afdb26c78fa3674eb5bb5c4adfca9a37348
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-08 15:43:49 +0300

    Update uploadFilesToS3.groovy

--- vars/uploadFilesToS3.groovy
+++ vars/uploadFilesToS3.groovy
@@ -1,4 +1,4 @@
-def call(Map stageParams) {
+def call(Map stageParams = [Region: "us-east-1", s3Bucket: "test-bucket-10082021", s3Path: "/"])) {
     withAWS(region: stageParams.awsRegion, credentials:'cloud_user') {
         awsIdentity()
         s3Upload( 
