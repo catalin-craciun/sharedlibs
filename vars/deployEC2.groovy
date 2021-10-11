db61a2ad1996637c4780ba5047c74e013088c381
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-11 13:10:55 +0300

    Update deployEC2.groovy

--- vars/deployEC2.groovy
+++ vars/deployEC2.groovy
@@ -1,6 +1,6 @@
 def call(){
   script{
-    sh 'aws cloudformation create-stack --stack-name EC2Jenkins --template-url s3://test-bucket-10112021/deployEC2.yml'
+    sh 'aws cloudformation create-stack --stack-name EC2Jenkins --template-url https://test-bucket-10112021.s3.amazonaws.com/deployEC2.yml'
   }
 }
 
