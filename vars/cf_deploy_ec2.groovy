9c696d6ce8b818178d52c8fac2adc846eaa38fd7
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-11 12:08:18 +0300

    Create cf_deploy_ec2.groovy

---
+++ vars/cf_deploy_ec2.groovy
@@ -0,0 +1,18 @@
+def call() {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Audit Tools') {                  
+                steps {
+                    auditTools()
+                }
+            }
+        stage('Build') {                  
+                steps {
+                    build_app()
+                }
+            }
+        }
+    }
+}
