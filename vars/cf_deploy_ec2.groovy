ca6660e4a304fc76b99ab20486472f0539b705c6
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-11 12:24:48 +0300

    Update cf_deploy_ec2.groovy

--- vars/cf_deploy_ec2.groovy
+++ vars/cf_deploy_ec2.groovy
@@ -1,6 +1,9 @@
 def call() {
     pipeline {
         agent any
+        environment {
+            AWS_CRED = 'cloud_user'
+        }
 
         stages {
             stage('Upload template to S3') {                  
