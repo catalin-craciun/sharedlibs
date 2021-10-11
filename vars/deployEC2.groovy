89a89ba3e2c25ab628130fe19e9b290c077c1c28
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-11 13:03:06 +0300

    Update deployEC2.groovy

--- vars/deployEC2.groovy
+++ vars/deployEC2.groovy
@@ -1,5 +1,6 @@
 def call(){
   script{
+    sh 'aws configure set region us-east-1'
     sh 'aws cloudformation create-stack --stack-name EC2Jenkins --template-url https://test-bucket-10112021.s3.amazonaws.com/deployEC2.yml'
   }
 }
