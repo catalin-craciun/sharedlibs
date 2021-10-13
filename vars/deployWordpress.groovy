ef2fb2806c9e8c1ce0d351cfa18f514d5d5bbf1a
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-13 12:34:10 +0300

    Create deployWordpress.groovy

---
+++ vars/deployWordpress.groovy
@@ -0,0 +1,14 @@
+def call(Map stageParams = [awsRegion: "us-east-1"]){
+  withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
+        //awsIdentity()
+        cfnCreateChangeSet(
+          stack:'Wordpress', 
+          changeSet:'my-change-set', 
+          url:'https://testbucket-catalinc-13102021.s3.amazonaws.com/deployEC2.yml'
+        )
+        cfnExecuteChangeSet(
+          stack:'Worspress', 
+          changeSet:'my-change-set'
+        )
+  }
+}
