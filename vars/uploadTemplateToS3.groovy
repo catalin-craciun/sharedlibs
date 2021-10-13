b85f546f4529147eb9ecd8732c731e0dac5b9955
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-13 09:39:08 +0300

    Update uploadTemplateToS3.groovy

--- vars/uploadTemplateToS3.groovy
+++ vars/uploadTemplateToS3.groovy
@@ -1,6 +1,6 @@
-def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "test-bucket-10112021"]) {
+def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-catalin-13102021"]) {
     withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
-        awsIdentity()
+        //awsIdentity()
         s3Upload( 
             includePathPattern: "*.yml",
             bucket: stageParams.s3Bucket
