de5e7e3230575acc615a215c90d87bc3e3aa749c
catalin-craciun <91620701+catalin-craciun@users.noreply.github.com>
2021-10-13 09:39:36 +0300

    Update deployEC2.groovy

--- vars/deployEC2.groovy
+++ vars/deployEC2.groovy
@@ -1,10 +1,10 @@
 def call(Map stageParams = [awsRegion: "us-east-1"]){
   withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
-        awsIdentity()
+        //awsIdentity()
         cfnCreateChangeSet(
           stack:'EC2Jenkins', 
           changeSet:'my-change-set', 
-          url:'https://test-bucket-10112021.s3.amazonaws.com/deployEC2.yml'
+          url:'https://testbucket-catalin-13102021.s3.amazonaws.com/deployEC2.yml'
         )
         cfnExecuteChangeSet(
           stack:'EC2Jenkins', 
